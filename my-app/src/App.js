d19487356be3239f275b33d2373040d7ef8199b0
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-08-16 15:23:27 +0900

    2023.08.16_이벤트

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function Header(props){
-  console.log('props', props, props.title);
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
-  </header>
+  <h1><a href="/" onClick={(event)=>{
+    event.preventDefault();
+    props.onChangeMode();
+  }}>{props.title}</a></h1>
+</header>
 }
 function Nav(props){
   const lis = []
   for(let i=0; i<props.topics.length; i++){
+
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+
+    lis.push(
+    <li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}
+      </a>
+
+    </li>)
   }
   return <nav>
     <ol>
@@ -41,8 +53,12 @@ function App() {
 
   return (
     <div>
-      <Header title={header_title}></Header>
-      <Nav topics={topics}></Nav>
+      <Header title={header_title} onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title={article_data.title} body={article_data.body}></Article>
     </div>
   );
