ea2bd453790f59437c7ba9d1e7641d52c9845b94
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-08-16 16:46:44 +0900

    2023.08.16_state값 활용

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+ 
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
 
 function Header(props){
   return <header>
-  <h1><a href="/" onClick={(event)=>{
-    event.preventDefault();
-    props.onChangeMode();
-  }}>{props.title}</a></h1>
-</header>
+    <h1><a href="/" onClick={(event)=>{
+      // event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
 }
+
 function Nav(props){
   const lis = []
   for(let i=0; i<props.topics.length; i++){
-
     let t = props.topics[i];
-
-    lis.push(
-    <li key={t.id}>
+    lis.push(<li key={t.id}>
       <a id={t.id} href={'/read/'+t.id} onClick={event=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id);
-      }}>{t.title}
-      </a>
-
+        props.onChangeMode(Number(event.target.id));
+      }}>{t.title}</a>
     </li>)
   }
   return <nav>
@@ -31,37 +35,44 @@ function Nav(props){
     </ol>
   </nav>
 }
-function Article(props){
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
-}
 
 function App() {
-  const header_title = 'WEB'
-
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javascript', body:'javascript is ...'}
-  ]
-
-  const article_data = {
-    title:'welcome', body:'Hi, WEB'
+    {id:3, title:'javascript', body:'javascript is ...'},
+    {id:4, title:'test', body:'test is ...'}
+  ] 
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
   }
-
   return (
     <div>
-      <Header title={header_title} onChangeMode={()=>{
-        alert('Header');
+      <Header title="WEB" onChangeMode={()=>{
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title={article_data.title} body={article_data.body}></Article>
+
+      {content}
+
     </div>
   );
 }
-
+ 
 export default App;
