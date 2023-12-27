280edfd12933f0d26ffaa3d978881640258c32bb
ParkJeeHoon <gkstj8300@naver.com>
2023-12-27 15:31:50 +0900

    유데미 Section.49 props를 통해 데이터 전달하기

--- 01-starting-setup/01-starting-setup/src/components/Expenseltem.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenseltem.js
@@ -1,17 +1,12 @@
 import './Expenseltem.css'
 
-const ExpenseItem = () => {
-
-    const expenseDate = new Date(2023, 12, 27);
-    const expenseTittle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
+const ExpenseItem = (props) => {
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTittle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
