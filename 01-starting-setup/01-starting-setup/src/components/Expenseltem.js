e3e5d8e2e1139d694507748ba58d2034148372eb
ParkJeeHoon <gkstj8300@naver.com>
2023-12-27 14:50:15 +0900

    유데미 Section 48 JSX에서 동적 데이터 출력하기 및 표현식 작업하기

--- 01-starting-setup/01-starting-setup/src/components/Expenseltem.js
+++ 01-starting-setup/01-starting-setup/src/components/Expenseltem.js
@@ -1,12 +1,17 @@
 import './Expenseltem.css'
 
 const ExpenseItem = () => {
+
+    const expenseDate = new Date(2023, 12, 27);
+    const expenseTittle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>March 28th 2023</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{expenseTittle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
