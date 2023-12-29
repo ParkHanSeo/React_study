8107c61927fedf4a9705946fbb0a892f149e58ad
ParkJeeHoon <gkstj8300@naver.com>
2023-12-29 17:45:20 +0900

    유데미 React 80. 불변의 객체 State(상태)로 업데이트하기

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,17 +1,37 @@
-const initialGameBoard = Array(3).fill([null, null, null]);
+import { useState } from 'react';
+
+// const initialGameBoard = Array(3).fill([null, null, null]);
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
 
 export const GameBoard = () => {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    const handleSelectSquare = (rowIndex, colIndex) => {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = "X";
+            console.log(updatedBoard);
+            return updatedBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIdx) => <li key={rowIdx}>
-                <ol>
-                    {row.map((playerSymbol, colIdx) => (
-                        <li key={colIdx}>
-                            <button>{playerSymbol}</button>
-                        </li>
-                    ))}
-                </ol>
-            </li>)}
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
         </ol>
     );
 }
