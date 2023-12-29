56c749c39161a409aa740617be773600849be3fd
ParkJeeHoon <gkstj8300@naver.com>
2023-12-29 14:46:08 +0900

    유데미 React 76. 조건적 콘텐츠 & State(상태) 업데이트를 위한 차선책

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -5,13 +5,15 @@ export const Player = ({name, symbol}) => {
     const [ isEditing, setIsEditing ] = useState(false);
 
     const handleEditClick = () => {
-        setIsEditing(true);
+        setIsEditing(!isEditing);
     }
 
     let playerName = <span className="player-name">{name}</span>;
+    // let btnCaption = "Edit";
 
     if(isEditing) {
-        playerName = <input type="text" required />
+        playerName = <input type="text" required value={name} />;
+        // btnCaption = "Save";
     }
 
     return (
@@ -20,7 +22,7 @@ export const Player = ({name, symbol}) => {
                 {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
