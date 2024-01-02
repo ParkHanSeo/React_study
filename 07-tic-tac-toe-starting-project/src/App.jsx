a54bafce98360fbcc906df0c2d944526611774ea
ParkJeeHoon <gkstj8300@naver.com>
2024-01-02 13:38:26 +0900

    유데미 React 90. 계산된 값에서 새로운 계산된 값 파생하기

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -31,20 +31,30 @@ function App() {
   let gameBoard = initialGameBoard;
 
   for (const turn of gameTurns) {
-      const { square, player } = turn;
-      const { row, col } = square;
+    const { square, player } = turn;
+    const { row, col } = square;
 
-      gameBoard[row][col] = player;
+    gameBoard[row][col] = player;
   }
 
-  for(const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol
-    const secondSquareSymbol
-    const thirdSquareSymbol
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
   }
 
   const handleSelectSquare = (rowIndex, colIndex) => {
-    
+
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
 
     setGameTurns((prevTurns) => {
@@ -74,6 +84,7 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
+        {winner && <p>You win {winner}</p>}
         <GameBoard
           onSelectSquare={handleSelectSquare}
           board={gameBoard} />
