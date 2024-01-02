541a3fc6619d5926c98cff8ce8dfda14af67aab4
ParkJeeHoon <gkstj8300@naver.com>
2024-01-02 13:32:41 +0900

    유데미 React 89. 계산된 값 끌어올리기

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,8 +3,13 @@ import { useState } from 'react';
 import { Player } from './components/Player'
 import { GameBoard } from './components/GameBoard'
 import { Log } from './components/Log'
-
 import { WINNING_COMBINATIONS } from './winning-combinations.js'
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
 
 const deriveActivePlayer = (gameTurns) => {
   let currentPlayer = 'X';
@@ -18,11 +23,28 @@ const deriveActivePlayer = (gameTurns) => {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  // const [hasWinner, setHasWinner] = useState(false);
   // const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player;
+  }
+
+  for(const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol
+    const secondSquareSymbol
+    const thirdSquareSymbol
+  }
+
   const handleSelectSquare = (rowIndex, colIndex) => {
+    
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
 
     setGameTurns((prevTurns) => {
@@ -54,7 +76,7 @@ function App() {
         </ol>
         <GameBoard
           onSelectSquare={handleSelectSquare}
-          turns={gameTurns} />
+          board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
