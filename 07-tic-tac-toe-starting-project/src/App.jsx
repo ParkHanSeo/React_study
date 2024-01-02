49e1cfbb0ab2335196668c0d5be4d68c1fa5219d
ParkJeeHoon <gkstj8300@naver.com>
2024-01-02 11:33:39 +0900

    유데미 React 86. State(상태) 관리 간소화 및 불필요한 State(상태) 분별

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,18 +4,28 @@ import { Player } from './components/Player'
 import { GameBoard } from './components/GameBoard'
 import { Log } from './components/Log'
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   const handleSelectSquare = (rowIndex, colIndex) => {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
-    setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
 
-      if(prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+    setGameTurns((prevTurns) => {
+      let currentPlayer = deriveActivePlayer(prevTurns);
+
       const updatedTurns = [
         { square: {row: rowIndex, col : colIndex}, player: currentPlayer }, 
         ...prevTurns
