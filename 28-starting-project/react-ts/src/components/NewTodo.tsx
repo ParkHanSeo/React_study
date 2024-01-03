bbcd0c2f8791dc66d69e78427cda84ba932ffe73
ParkJeeHoon <gkstj8300@naver.com>
2024-01-03 16:39:46 +0900

    유데미 React 615. refs 및 useRef 작업하기

--- 28-starting-project/react-ts/src/components/NewTodo.tsx
+++ 28-starting-project/react-ts/src/components/NewTodo.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export const NewTodo = () => {
 
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
+
+        const enteredText = todoTextInputRef.current?.value;
+
+        if(enteredText?.trim().length === 0) {
+            return;
+        }
+
+        return enteredText;
     };
 
 
     return (
         <form onSubmit={submitHandler}>
             <label htmlFor="text">Todo text</label>
-            <input type="text" id='text'/>
+            <input type="text" id='text' ref={todoTextInputRef}/>
             <button>Add Todo</button>
         </form>
     );
