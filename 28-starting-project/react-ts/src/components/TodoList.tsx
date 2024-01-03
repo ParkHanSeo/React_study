631ce683598164edd05163dac0230e774eae727c
ParkJeeHoon <gkstj8300@naver.com>
2024-01-03 17:29:58 +0900

    유데미 React 618. 스타일링 추가하기

--- 28-starting-project/react-ts/src/components/TodoList.tsx
+++ 28-starting-project/react-ts/src/components/TodoList.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './TodoItem.module.scss'
 
 import { Todo } from '../domain/todo'
 
 export const TodoList: React.FC<{ text: string }> = (props) => {
     return (
-        <li>{props.text}</li>
+        <li className={styles.item}>{props.text}</li>
     );
 }
--- 28-starting-project/react-ts/src/components/Todos.tsx
+++ 28-starting-project/react-ts/src/components/Todos.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import { Todo } from '../domain/todo'
 import { TodoList } from './TodoList'
+import styles from './Todos.module.scss'
 
 export const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
-        <ul>
+        <ul className={styles.todos}>
             {props.items.map((data) => (
                 <TodoList key={data.id} text={data.text}/>
             ))}
