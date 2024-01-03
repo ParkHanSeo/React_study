642353b10d3e2b9f9c59d066052b2b1c41a1466c
ParkJeeHoon <gkstj8300@naver.com>
2024-01-03 15:14:17 +0900

    유데미 React 611. Props 및 TypeScript 작업하기

--- 28-starting-project/react-ts/src/components/Todos.tsx
+++ 28-starting-project/react-ts/src/components/Todos.tsx
@@ -1,8 +1,11 @@
-export const Todos = () => {
+import React from 'react';
+
+export const Todos: React.FC<{ items: string[] }> = (props) => {
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Learn TypeScript</li>
+            {props.items.map((data) => (
+                <li>{data}</li>
+            ))}
         </ul>
     );
 }
