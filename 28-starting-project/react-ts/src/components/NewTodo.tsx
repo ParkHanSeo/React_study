10774f01b804befde1ca84cdb591df6699be4320
ParkJeeHoon <gkstj8300@naver.com>
2024-01-03 16:52:50 +0900

    유데미 React 616. "Function Props"로 작업하기

--- 28-starting-project/react-ts/src/components/NewTodo.tsx
+++ 28-starting-project/react-ts/src/components/NewTodo.tsx
@@ -1,19 +1,19 @@
 import React, { useRef } from "react";
 
-export const NewTodo = () => {
+export const NewTodo: React.FC<{ onAddTodo: (text:string) => void }> = (props) => {
 
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
 
-        const enteredText = todoTextInputRef.current?.value;
+        const enteredText = todoTextInputRef.current!.value;
 
         if(enteredText?.trim().length === 0) {
             return;
         }
 
-        return enteredText;
+        props.onAddTodo(enteredText);
     };
 
 
