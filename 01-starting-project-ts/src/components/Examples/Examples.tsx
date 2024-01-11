a0c6fc64dc6ea37954ebe476fb5dcec56bf2f0fa
parkjeehoon <gkstj8300@naver.com>
2024-01-11 22:04:25 +0900

    React 01 Project TS Tabs 진행중

--- 01-starting-project-ts/src/components/Examples/Examples.tsx
+++ 01-starting-project-ts/src/components/Examples/Examples.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { EXAMPLES } from '../../data';
+
 export const Examples: React.FC<{}> = ({}) => {
     return (
         <></>
---
+++ 01-starting-project-ts/src/components/TabButton/Tabs.tsx
@@ -0,0 +1,6 @@
+export const Tabs : React.FC<{}> = () => {
+
+    return (
+        <></>
+    );
+}
