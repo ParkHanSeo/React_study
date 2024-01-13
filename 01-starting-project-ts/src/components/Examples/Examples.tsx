edb1205afc880dfeec596ff8c8865b5c1c2fa875
parkjeehoon <gkstj8300@naver.com>
2024-01-13 23:14:43 +0900

    React 01 Project TS Section 진행중

--- 01-starting-project-ts/src/components/Examples/Examples.tsx
+++ 01-starting-project-ts/src/components/Examples/Examples.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { EXAMPLES } from '../../data';
+import { TabButton } from '../TabButton/TabButton';
+import { Tabs } from '../TabButton/Tabs';
 
 export const Examples: React.FC<{}> = ({}) => {
     return (
--- 01-starting-project-ts/src/components/Section.tsx
+++ 01-starting-project-ts/src/components/Section.tsx
@@ -1,4 +1,8 @@
-export const Section = ({ title, children, ...props }) => {
+import React from "react";
+
+
+
+export const Section: React.FC<{}> = ({ title, children, ...props }) => {
     return (
         <section {...props}>
             <h2>{title}</h2>
