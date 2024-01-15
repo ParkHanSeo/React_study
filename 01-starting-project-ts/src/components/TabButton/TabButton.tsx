a9a40045ecd48a653f99d51aaabd3a00d53279ba
parkjeehoon <gkstj8300@naver.com>
2024-01-15 23:01:16 +0900

    React 01 Project TS Tabbutton 생성

--- 01-starting-project-ts/src/components/TabButton/TabButton.tsx
+++ 01-starting-project-ts/src/components/TabButton/TabButton.tsx
@@ -3,6 +3,10 @@ import React from "react";
 
 export const TabButton: React.FC<{}> = ({}) => {
     return (
-        <></>
+        <li>
+            <button>
+                
+            </button>
+        </li>
     );
 };
