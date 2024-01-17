b0b5723c8ad4045dee83f67705faf634a25c8d01
ParkJeeHoon <gkstj8300@naver.com>
2024-01-17 18:11:33 +0900

    test: test 파일 생성

---
+++ 03-react-ts/component/Test.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+
+type Props = {
+    children: ReactNode
+}
+
+export const Test: React.FC<Props> = ({ children }) => {
+
+    return (
+        <></>
+    );
+};
