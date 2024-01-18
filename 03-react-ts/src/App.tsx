41f12a53e2516cbbb89a56d31f10492d760333c1
ParkJeeHoon <gkstj8300@naver.com>
2024-01-18 14:13:17 +0900

    feat: index css 추가

--- 03-react-ts/src/App.tsx
+++ 03-react-ts/src/App.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
+import { TPLACES, AVAILABLE_PLACES } from './data';
 import { Header } from './component/Header/Header';
-
-import logoImg from './assets/logo.png';
+import { Modal } from './component/Modal/Modal';
+import { Places } from './component/Places/Places';
 
 function App() {
+  const modal = useRef(null);
+  const selectedPlace = useRef<string>();
+  const [pickedPlaces, setPickedPlaces] = useState<TPLACES[]>([]);
+
+  const handleStartRemovePlace = (id: string) => {
+    selectedPlace.current = id;
+  }
+
+  const handleSelectPlace = (id: string) => {
+    setPickedPlaces((prevPickedPlaces) => {
+      const alreadySelected = prevPickedPlaces.some((place) => place.id === id);
+      if (alreadySelected) {
+        return prevPickedPlaces;
+      } else {
+        return prevPickedPlaces;
+      }
+    });
+  }
+
   return (
     <>
       <Header />
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={'Select the places you would like to visit below.'}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title="Available Places"
+          places={AVAILABLE_PLACES}
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
     </>
   );
 }
