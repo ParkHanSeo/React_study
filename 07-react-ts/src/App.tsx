c99d0250aa7edf7585a20d34b66316c3d444d55a
ParkJeeHoon <gkstj8300@naver.com>
2024-01-19 10:00:24 +0900

    feat: header 생성

--- 07-react-ts/src/App.tsx
+++ 07-react-ts/src/App.tsx
@@ -1,11 +1,17 @@
 import React, { useRef, useState } from 'react';
 
+import styles from './App.module.scss';
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <main>
+      <div className={styles.gameContainer}>
+        <ol className={styles.players}>
+
+        </ol>
+      </div>
+    </main>
   );
 }
 
