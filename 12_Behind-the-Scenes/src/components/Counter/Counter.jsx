b49a4a671a8de70f7d1a7e8cbde7611e66170579
ParkJeeHoon <gkstj8300@naver.com>
2024-01-22 18:01:52 +0900

    feat: useMemo 사용

--- 12_Behind-the-Scenes/src/components/Counter/Counter.jsx
+++ 12_Behind-the-Scenes/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo } from 'react';
+import { useState, useMemo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
