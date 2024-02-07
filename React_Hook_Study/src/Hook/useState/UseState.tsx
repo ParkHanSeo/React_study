a9935f810e49d65a3ec1bfb961dd6cbdc6b74021
ParkJeeHoon <gkstj8300@naver.com>
2024-02-07 18:01:02 +0900

    feat: UseState module 생성

--- React_Hook_Study/src/Hook/useState/UseState.tsx
+++ React_Hook_Study/src/Hook/useState/UseState.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
+import styles from './UseState.module.scss';
 
 export const UseState: React.FC<{}> = () => {
 
     const [count, setCount] = useState<number>(0);
     const [text, setText] = useState('hello');
+    const [form, setForm] = useState({
+        firstName: 'Barbara',
+        lastName: 'Hepworth',
+        email: 'bhepworth@sculpture.com',
+    });
 
     const handleClick = () => {
         setCount(count + 1);
@@ -15,18 +21,71 @@ export const UseState: React.FC<{}> = () => {
 
     return (
         <div>
-            <div>
-                <button onClick={handleClick}>
-                    카운트를 증가 {count}
-                </button>
-            </div>
-            <div>
-                <input value={text} onChange={handleChange} />
-                <p>You typed: {text}</p>
-                <button onClick={() => setText('hello')}>
-                    Reset
-                </button>
-            </div>
+            <ul className={styles.useStateUl}>
+                <li>
+                    <h3>useState_1</h3>
+                    <button onClick={handleClick}>
+                        카운트를 증가 {count}
+                    </button>
+                </li>
+                <li>
+                    <h3>useState_2</h3>
+                    <input value={text} onChange={handleChange} />
+                    <p>You typed: {text}</p>
+                    <button onClick={() => setText('hello')}>
+                        Reset
+                    </button>
+                </li>
+                <li>
+                    <div className={styles.formDiv}>
+                        <label>
+                            First name:
+                            <input
+                            value={form.firstName}
+                            onChange={e => {
+                                setForm({
+                                ...form,
+                                firstName: e.target.value
+                                });
+                            }}
+                            />
+                        </label>
+                        <label>
+                            Last name:
+                            <input
+                            value={form.lastName}
+                            onChange={e => {
+                                setForm({
+                                ...form,
+                                lastName: e.target.value
+                                });
+                            }}
+                            />
+                        </label>
+                        <label>
+                            Email:
+                            <input
+                            value={form.email}
+                            onChange={e => {
+                                setForm({
+                                ...form,
+                                email: e.target.value
+                                });
+                            }}
+                            />
+                        </label>
+                        <div className={styles.pDiv}>
+                            <p>
+                                {form.firstName}{' '}
+                                {form.lastName}{' '}
+                            </p>
+                            <p>
+                                ({form.email})
+                            </p>
+                        </div> 
+                    </div>
+                </li>
+            </ul>
         </div>
     )
 }
